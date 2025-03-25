57b4f39acd9397a13f1ac650f1d53d5484d554ea
miguelito2122 <miguelgremio@gmail.com>
2025-03-25 03:40:54 -0300

    Commit de Segurança para testar update.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from root import Root
-import subprocess
-import sys
 
-def atualizar_aplicativo():
-    subprocess.run([sys.executable, 'update.py'])
-    
+
 if __name__ == '__main__':
     app = Root()
     app.mainloop()
--- app/root.py
+++ app/root.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from notebook import Notebook
 from update import update_application
+import subprocess
+import sys
 
 class Root(tk.Tk):
     def __init__(self):
@@ -11,8 +13,10 @@ class Root(tk.Tk):
         self.resizable(False, False)
         self.centralizar_tela(self)
         # Notebook
-        self.atualizar = update_application()
+        self.atualizar_aplicativo()
         self.notebook = Notebook(self)
+    def atualizar_aplicativo():
+        subprocess.run([sys.executable, 'update.py'])
     def centralizar_tela(self, tela):
         tela.update_idletasks()
 
